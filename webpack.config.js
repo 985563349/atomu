ee0a4701835d61082dc531b2ae9a3feffeb44b95
王杰 <jie985563349@outlook.com>
2024-03-07 10:38:43 +0800

    build: add babel

--- webpack.config.js
+++ webpack.config.js
@@ -12,4 +12,18 @@ module.exports = {
     },
     clean: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
 };
