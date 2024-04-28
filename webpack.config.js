04bf3e2d03d4bdcb34da37234d44830e595262bb
王杰 <jie985563349@outlook.com>
2024-04-28 14:55:10 +0800

    build: update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  devtool: 'source-map',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
